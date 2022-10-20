f8704518f996f58bf87886af6bb2b29b3ef9cde6
Yohan Manuel Estevez Rueda <estevezruedayohan@gmail.com>
2022-10-20 14:59:22 -0500

    [add] Procfile

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ const {
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+const whiteList = ['http://localhost:8080', 'https://myapp.co'];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+
+app.use(cors(options));
 
 routerApi(app);
 
